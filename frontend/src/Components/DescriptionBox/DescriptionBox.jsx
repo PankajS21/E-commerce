c9c2c99d8a130529875dfff92be41bcb52692a59
Pankaj Singh <psingh2@univoedtech.com>
2024-07-30 11:40:44 +0530

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./DescriptionBox.css";
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ipsam
+          mollitia rerum in fuga suscipit molestias omnis explicabo hic, beatae
+          officia! Delectus vitae, et quae fugiat obcaecati est pariatur nobis
+          minus hic voluptate natus magni vero animi corporis placeat, maxime
+          iste nam ex deserunt nisi nemo voluptas! Fugit ut, itaque dolores
+          similique, tempora in labore porro cum non repudiandae quis odit
+          deleniti nulla nemo eos, consequatur pariatur ipsum architecto eum
+          enim expedita consectetur reprehenderit! Perspiciatis voluptatem vel
+          facilis nemo? Vel consequuntur necessitatibus, quam minima suscipit id
+          perferendis eos architecto asperiores voluptas illo molestiae quidem
+          officia consequatur possimus impedit dolores magnam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore vel,
+          ipsa accusantium culpa qui voluptatibus, ipsam sit perferendis
+          laudantium odit temporibus, enim tenetur. Ab non, adipisci
+          voluptatibus doloremque reprehenderit rerum laudantium quidem tempore
+          laboriosam earum cupiditate numquam harum perferendis labore accusamus
+          molestias ipsam voluptate. Sit dolores autem voluptate, a amet
+          veritatis corporis minus, eligendi corrupti culpa hic aut, voluptatem
+          molestias.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
---
+++ frontend/src/Components/Popular/Popular.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Popular.css";
+import Item from "../Item/Item";
+// import data_product from "../Assets/data";
+
+const Popular = () => {
+  const [popularProducts, setPopularProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/popularinwomen")
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data));
+  }, []);
+  return (
+    <div className="popular">
+      <h1>POPULAR IN WOMEN</h1>
+      <hr />
+      <div className="popular-item">
+        {popularProducts.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              old_price={item.old_price}
+              new_price={item.new_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Popular;
